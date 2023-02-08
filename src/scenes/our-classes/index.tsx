c7d8d00a339eee0481e53473fdf0432b9ce8552b
pulsar1812 <cluster000@gmail.com>
2023-02-08 22:04:46 +0800

    Finish OurClasses

--- src/scenes/our-classes/index.tsx
+++ src/scenes/our-classes/index.tsx
@@ -7,15 +7,91 @@ import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
+import HeaderText from "@/shared/HeaderText";
+import OurClass from "./OurClass";
+import { ClassType } from "@/shared/types";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
+const classList: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image6,
+  },
+];
+
 export default function OurClasses({ setSelectedPage }: Props) {
   return (
     <section id="ourclasses" className="w-full bg-primary-100 py-40">
-      OurClasses
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HeaderText>Our Classes</HeaderText>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+              provident adipisci non veritatis unde eius eum error quam atque
+              distinctio est accusamus iusto saepe ex, quae delectus officia
+              assumenda voluptatem voluptas deserunt temporibus voluptatum
+              labore.
+            </p>
+          </div>
+        </motion.div>
+
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classList.map((item: ClassType, index) => (
+              <OurClass
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
     </section>
   );
 }
