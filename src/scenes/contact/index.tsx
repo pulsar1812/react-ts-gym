c09df17736f2c7525ca8fdef9d3795dd271fae5a
pulsar1812 <cluster000@gmail.com>
2023-02-13 00:10:47 +0800

    Finish Contact section

--- src/scenes/contact/index.tsx
+++ src/scenes/contact/index.tsx
@@ -10,6 +10,9 @@ type Props = {
 };
 
 export default function Contact({ setSelectedPage }: Props) {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+    px-5 py-3 placeholder-white`;
+
   const {
     register,
     trigger,
@@ -70,8 +73,86 @@ export default function Contact({ setSelectedPage }: Props) {
               target="_blank"
               onSubmit={handleSubmit}
             >
-              <input />
+              <input
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+                className={inputStyles}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
+
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+                className={inputStyles}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                rows={4}
+                cols={50}
+                placeholder="Message"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+                className={inputStyles}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 characters."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                Submit
+              </button>
             </form>
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="relative mt-16 basis-2/5 md:mt-0"
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                src={ContactUsPageGraphic}
+                alt="contact us"
+                className="w-full"
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
